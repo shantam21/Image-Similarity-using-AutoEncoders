f0b23bb808cd6fbd245a99ce0acd6144ed921627
shantam <shantam.saxena@gmail.com>
2020-09-05 01:46:10 +0530

    Add files via upload

---
+++ clustering.py
@@ -0,0 +1,48 @@
+from keras import backend as K
+from sklearn.cluster import KMeans
+from sklearn.metrics import confusion_matrix
+import numpy as np
+import pandas as pd
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import load_model
+from pathlib import Path
+from yellowbrick.cluster import KElbowVisualizer
+
+
+if __name__ == '__main__':
+    train_directory = 'data/train'
+    X = np.load('image_encodings.npy')
+    model = load_model('model.h5')
+    get_encoded = K.function([model.layers[0].input], [model.layers[4].output])
+    X_encoded = np.empty((len(X), 56, 56, 16), dtype='float32')
+    step = 100
+    for i in range(0, len(X), step):
+        x_batch = get_encoded([X[i:i+step]])[0]
+        X_encoded[i:i+step] = x_batch
+
+    print(X_encoded.shape)
+    X_encoded_reshape = X_encoded.reshape(X_encoded.shape[0], X_encoded.shape[1]*X_encoded.shape[2]*X_encoded.shape[3])
+    print(X_encoded_reshape.shape)
+    df = pd.DataFrame(X_encoded_reshape)
+    dir = Path(train_directory)
+    images = dir.glob('*jpg')
+    images = [el for el in images]
+    
+    df['image_name'] = images
+    # df.to_csv('op_encoded.csv')
+    
+    pca = PCA(0.95)
+    x = df.drop(['image_name'], axis= 1)
+    pca.fit(x)
+    x = pca.transform(x)
+    # kmeans_model = KElbowVisualizer(KMeans(), k=10)
+    # kmeans_model.fit(x)
+    # kmeans_model.show()
+    km = KMeans(n_clusters=5)
+    km.fit(x)
+    clustering_result = pd.DataFrame(columns = ['Image', 'Label'])
+    clustering_result['Image'] = images
+    clustering_result['Label'] = km.labels_
+    clustering_result.to_csv('clusters.csv')
+     
---
+++ main.py
@@ -0,0 +1,84 @@
+import numpy as np
+from read_images import read_images
+from model import autoencoder_model
+from model import train_model
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.losses import CosineSimilarity
+from pathlib import Path
+import matplotlib.pyplot as plt
+from PIL import Image
+import glob
+
+def plot_top_n_matches(no_of_matches,image_similarities):
+    """
+    This function plots the top-n images for the test image.
+
+    Args:
+        no_of_matches (int): no of matches required
+        image_similarities (dict): Image and the similarity w.r.t test image.
+    """    
+    top_n_matches = sorted(image_similarities.items(), key=lambda x: x[1])[:no_of_matches]
+    image_names = [el[0].replace('\\','/') for el in top_n_matches]
+    dir = Path(test_directory)
+    test_image = dir.glob('*jpg')
+    test_image = [el for el in test_image][0]
+    rows = round(no_of_matches/5)
+    columns = 5
+    fig, ax = plt.subplots(rows, columns)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(np.asarray(Image.open(test_image)))
+    i = 0
+    for x in range(rows):
+        for y in range(columns):
+            ax[x, y].set_xticks([])
+            ax[x, y].set_yticks([])
+            if i >= len(image_names):
+                ax[x, y].axis('off')
+            else:
+                ax[x, y].imshow(np.asarray(Image.open(image_names[i])))
+            i += 1
+    plt.show()
+               
+               
+
+if __name__ == '__main__':
+    
+    #  configurations for the script
+    no_of_matches = 10
+    
+    # Image directories
+    train_directory = 'data/train'
+    test_directory = 'data/test'
+    
+    # Read images into np arrays
+    read_images(train_directory)
+    read_images(test_directory, test = True)
+    X = np.load("X.npy")
+    
+    #  Model training
+    train_model(X, epochs = 1, bs = 64, validation_split = 0.2)
+    model = load_model('trained_model.h5')
+    encoded_data = model.predict(X)
+    np.save('image_encodings.npy', encoded_data)
+    
+    encodings_train = np.load('image_encodings.npy')
+    encodings_test = np.load('X_test.npy')
+    dir = Path(train_directory)
+    imgs=dir.glob('*jpg')
+    names = [str(el) for el in imgs]
+    
+    #  Cosine similarities of test image w.r.t train images
+    similarities = []
+    cosine_sim = CosineSimilarity(axis = 1)
+    for i in range(encodings_train.shape[0]):
+        sim = cosine_sim(encodings_test[0], encodings_train[i]).numpy()
+        # print(sim)
+        similarities.append(sim)
+    
+    image_similarities = dict(zip(names, similarities))
+    plot_top_n_matches(no_of_matches,image_similarities)
+
+    
+    
